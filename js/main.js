94c501932e980b03622dca80455829de9435a400
Faith Ebert <missfaithannee@gmail.com>
2020-03-03 10:48:00 -0500

    Add Javascript code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-var name = prompt('Enter a letter');
+var letterPrompt = prompt('Enter a letter');
+
+for (prompt=0; prompt>10; prompt ++) {
+  document.write ('letterPrompt')
+  document.write ('<br>')
+}
